2811c532bbba94a3be033935d9c8f724befaeb24
Muayad Sawalhi <muayad.sawalhi@outlook.com>
2026-01-08 22:32:53 +0400

    updated themeToggle icon background (removed button)

--- assests/scripts/dark-light-mode.js
+++ assests/scripts/dark-light-mode.js
@@ -2,12 +2,13 @@
 
 // Access the DOM
 const toggle = document.getElementById("toggle-theme");
-
+const switchIcon = document.getElementById("themeToggle");
 // Action function
 function toggleTheme() {
   const currentTheme = document.body.getAttribute("data-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   document.body.setAttribute("data-theme", newTheme);
+  switchIcon = switchIcon.setAttribute("class", '"bi bi-brightness-high btn"');
 }
 
 // Event Listener
