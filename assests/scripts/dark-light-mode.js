59b6318ac334ca9134a5c3c83b30dfd773f38f3a
Muayad Sawalhi <muayad.sawalhi@outlook.com>
2026-01-10 20:52:51 +0400

    updated themeToggle script to switch theme icon in nav bar

--- assests/scripts/dark-light-mode.js
+++ assests/scripts/dark-light-mode.js
@@ -1,15 +1,25 @@
 // toggle dark/light mode
 
 // Access the DOM
-const toggle = document.getElementById("toggle-theme");
-const switchIcon = document.getElementById("themeToggle");
+let switchIcon = document.getElementById("themeToggle");
+const lightIcon = "bi bi-brightness-high btn";
+const darkIcon = "bi bi-moon-stars-fill btn";
+const darkTheme = "dark";
+const lightTheme = "light";
+
 // Action function
+
 function toggleTheme() {
   const currentTheme = document.body.getAttribute("data-theme");
-  const newTheme = currentTheme === "dark" ? "light" : "dark";
-  document.body.setAttribute("data-theme", newTheme);
-  switchIcon = switchIcon.setAttribute("class", '"bi bi-brightness-high btn"');
-}
+  // const newTheme = currentTheme === "dark" ? "light" : "dark";
 
-// Event Listener
-toggle.addEventListener("click", toggleTheme);
+  if (currentTheme === "dark") {
+    document.body.setAttribute("data-theme", lightTheme);
+    switchIcon.setAttribute("class", lightIcon);
+  }
+
+  if (currentTheme === "light") {
+    document.body.setAttribute("data-theme", darkTheme);
+    switchIcon.setAttribute("class", darkIcon);
+  }
+}
